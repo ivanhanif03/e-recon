ad409518d4d3babbed05c9276e59ed4654e932e2
shafiranuradya <shafiranuradya@gmail.com>
2022-11-03 23:24:52 +0700

    database

---
+++ app/Database/Migrations/2022-11-03-150618_CreateBwinet.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateBwinet extends Migration
+{
+    public function up()
+    {
+        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 25,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'Harga' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('bw_inet');
+    }
+
+    public function down()
+    {
+        //
+        $this->forge->dropTable('bw_inet');
+    }
+}
