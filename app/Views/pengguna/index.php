4603ac32069e03f0c068b11a55660680ee79236f
M Ivan Hanif V <ivanhanif03@gmail.com>
2022-11-14 17:00:04 +0700

    update pengguna

--- app/Views/pengguna/index.php
+++ app/Views/pengguna/index.php
@@ -9,7 +9,11 @@
                     <h1 class="mb-4">Daftar User</h1>
                     <!-- <h1 class="mb-4">Daftar User</h1> -->
                     <?= view('Myth\Auth\Views\_message_block') ?>
-
+                    <?php if (session()->getFlashdata('pesan')) : ?>
+                    <div class="alert alert-success" role="alert" id="alert-delete">
+                        <?= session()->getFlashdata('pesan'); ?>
+                    </div>
+                    <?php endif; ?>
                     <div class="card shadow-none border-none mt-2">
                         <div class="card-header">
                             <div class="row">
@@ -44,27 +48,68 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php $i=1; foreach ($user as $u) : ?>
+                                    <?php $i=1; foreach ($pengguna as $p) : ?>
                                     <tr>
                                         <td class="align-middle"><?= $i++; ?></td>
-                                        <!-- <td class="align-middle text-capitalize"><img src="/img/<?= $u['user_image']; ?>" class="img-circle" width="50" alt="User Image"></td> -->
-                                        <td class="align-middle text-capitalize"><?= $u['fullname']; ?></td>
-                                        <td class="align-middle"><?= $u['email']; ?></td>
-                                        <td class="align-middle"><?= $u['no_hp']; ?></td>
-                                        <!-- <td class="align-middle text-capitalize"><?= $u['alamat']; ?></td> -->
-                                        <td class="align-middle text-capitalize"><?= $u['provider']; ?></td>
-                                        <td class="align-middle text-capitalize"><?= $u['hak_akses']; ?></td>
+                                        <!-- <td class="align-middle text-capitalize"><img src="/img/<?= $p['user_image']; ?>" class="img-circle" width="50" alt="User Image"></td> -->
+                                        <td class="align-middle text-capitalize"><?= $p['fullname']; ?></td>
+                                        <td class="align-middle"><?= $p['email']; ?></td>
+                                        <td class="align-middle"><?= $p['no_hp']; ?></td>
+                                        <!-- <td class="align-middle text-capitalize"><?= $p['alamat']; ?></td> -->
+                                        <td class="align-middle text-capitalize"><?= $p['provider']; ?></td>
+                                        <td class="align-middle text-capitalize"><?= $p['hak_akses']; ?></td>
                                         <td class="text-center">
                                             <!-- Edit -->
-                                            <a href="#" class="btn btn-sm btn-outline-primary" data-toggle="modal"
-                                                data-backdrop="static" data-target="#modal-tambah-user"><i
+                                            <a href="/komik/edit/<?= $p['id']; ?>"
+                                                class="btn btn-sm btn-outline-primary" data-toggle="modal"
+                                                data-backdrop="static" data-target="#modal-edit-user"><i
                                                     class="nav-icon fas fa-edit"></i></a>
                                             <!-- Delete -->
+                                            <!-- <form action="/pengguna/<?= $p['id']; ?>" method="post" class="d-inline">
+                                                <?= csrf_field(); ?>
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <button type="submit" class="btn btn-sm btn-outline-danger"><i
+                                                        class=" nav-icon fas fa-trash"></i></button>
+                                            </form> -->
+
                                             <a href="#" class="btn btn-sm btn-outline-danger" data-toggle="modal"
-                                                data-backdrop="static" data-target="#modal-hapus-user"><i
+                                                data-target="#modal-hapus-pengguna<?= $p['id'] ?>"><i
                                                     class=" nav-icon fas fa-trash"></i></a>
                                         </td>
                                     </tr>
+
+                                    <!-- Start Modal Delete -->
+                                    <div class="modal fade" id="modal-hapus-pengguna<?= $p['id'] ?>">
+                                        <div class="modal-dialog modal-sm">
+                                            <div class="modal-content border-0">
+                                                <div class="modal-header">
+                                                    <h4 class="modal-title">Hapus Data</h4>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                        aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body text-center">
+                                                    <span>Anda yakin ingin menghapus data?</span><br>
+                                                    <span class="text-capitalize font-weight-bolder text-primary">
+                                                        <?= $p['fullname']; ?>
+                                                        -
+                                                        <?= $p['hak_akses']; ?></span>
+                                                </div>
+                                                <div class="modal-footer justify-content-between">
+                                                    <button type="button" class="btn btn-primary"
+                                                        data-dismiss="modal">Batal</button>
+                                                    <form action="/pengguna/<?= $p['id']; ?>" method="post">
+                                                        <?= csrf_field(); ?>
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <button type="submit" class="btn btn-danger">Hapus</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- End Modal Delete -->
+
                                     <?php endforeach; ?>
                                 </tbody>
                                 <tfoot>
@@ -90,7 +135,7 @@
     </div>
 </div>
 
-<!-- Modal Input -->
+<!-- Start Modal Input -->
 <div class="modal fade" id="modal-tambah-user">
     <div class="modal-dialog modal-md">
         <div class="modal-content border-0">
@@ -137,13 +182,15 @@
                                 <label for="fullname">Nama Lengkap</label>
                                 <input type="text"
                                     class="form-control text-sm <?php if (session('errors.fullname')) : ?>is-invalid<?php endif ?>"
-                                    name="fullname" id="fullname" placeholder="Masukkan nama lengkap" required>
+                                    name="fullname" id="fullname" placeholder="Masukkan nama lengkap"
+                                    value="<?= old('fullname') ?>" required>
                             </div>
                             <div class="form-group">
                                 <label for="no_hp">Nomor Handphone</label>
                                 <input type="text"
                                     class="form-control text-sm <?php if (session('errors.no_hp')) : ?>is-invalid<?php endif ?>"
                                     name="no_hp" id="no_hp" placeholder="Masukkan nomor handphone" maxlength="13"
+                                    value="<?= old('no_hp') ?>"
                                     oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');"
                                     required>
                             </div>
@@ -152,7 +199,17 @@
                                 <textarea
                                     class="form-control text-sm <?php if (session('errors.alamat')) : ?>is-invalid<?php endif ?>"
                                     name="alamat" id="alamat" rows="3" placeholder="Masukkan alamat"
-                                    required></textarea>
+                                    value="<?= old('alamat') ?>" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Hak Akses</label>
+                                <select
+                                    class="form-control text-sm <?php if (session('errors.hak_akses')) : ?>is-invalid<?php endif ?>"
+                                    name="hak_akses" id="hak_akses" style="width: 100%;">
+                                    <option disabled="disabled" selected="selected">Pilih Hak Akses</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">User</option>
+                                </select>
                             </div>
                             <div class="form-group">
                                 <label>Provider</label>
@@ -172,16 +229,6 @@
                                     <option value="bas">BAS</option>
                                 </select>
                             </div>
-                            <div class="form-group">
-                                <label>Hak Akses</label>
-                                <select
-                                    class="form-control text-sm <?php if (session('errors.hak_akses')) : ?>is-invalid<?php endif ?>"
-                                    name="hak_akses" id="hak_akses" style="width: 100%;">
-                                    <option disabled="disabled" selected="selected">Pilih Hak Akses</option>
-                                    <option value="admin">Admin</option>
-                                    <option value="user">User</option>
-                                </select>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -194,28 +241,102 @@
         </div>
     </div>
 </div>
+<!-- End Modal Input -->
 
-<!-- Modal Delete -->
-<div class="modal fade" id="modal-hapus-user">
-    <div class="modal-dialog modal-sm">
+<!-- Start Modal Edit -->
+<div class="modal fade" id="modal-edit-user">
+    <div class="modal-dialog modal-md">
         <div class="modal-content border-0">
             <div class="modal-header">
-                <h4 class="modal-title">Hapus Data</h4>
+                <h4 class="modal-title">Form Edit Pengguna</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <p>Anda yakin ingin menghapus data?</p>
-            </div>
-            <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Batal</button>
-                <button type="button" class="btn btn-danger " data-dismiss="modal">Hapus</button>
-                <!-- swalDeleteSuccess -->
-            </div>
+            <form action="/pengguna/update/<?= $p['id']; ?>" method="post">
+                <?= csrf_field(); ?>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label for="email"><?=lang('Auth.email')?></label>
+                                <input type="email"
+                                    class="form-control text-sm <?php if (session('errors.email')) : ?>is-invalid<?php endif ?>"
+                                    name="email" id="email" aria-describedby="emailHelp" placeholder="Masukkan email"
+                                    value="<?= $p['email']; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text"
+                                    class="form-control text-sm <?php if (session('errors.username')) : ?>is-invalid<?php endif ?>"
+                                    name="username" id="username" placeholder="Masukkan username"
+                                    value="<?= $p['username']; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="fullname">Nama Lengkap</label>
+                                <input type="text"
+                                    class="form-control text-sm <?php if (session('errors.fullname')) : ?>is-invalid<?php endif ?>"
+                                    name="fullname" id="fullname" placeholder="Masukkan nama lengkap"
+                                    value="<?= $p['fullname']; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="no_hp">Nomor Handphone</label>
+                                <input type="text"
+                                    class="form-control text-sm <?php if (session('errors.no_hp')) : ?>is-invalid<?php endif ?>"
+                                    name="no_hp" id="no_hp" placeholder="Masukkan nomor handphone" maxlength="13"
+                                    value="<?= $p['no_hp']; ?>"
+                                    oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label for="alamat">Alamat</label>
+                                <textarea
+                                    class="form-control text-sm <?php if (session('errors.alamat')) : ?>is-invalid<?php endif ?>"
+                                    name="alamat" id="alamat" rows="3" placeholder="Masukkan alamat"
+                                    required><?= $p['alamat']; ?></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Provider</label>
+                                <!-- <select class="form-control select2bs4 text-sm" name="provider" id="provider" -->
+                                <select
+                                    class="form-control text-sm <?php if (session('errors.provider')) : ?>is-invalid<?php endif ?>"
+                                    name="provider" id="provider" value="<?= $p['provider']; ?>" style="width: 100%;">
+                                    <option disabled="disabled" selected="selected">Pilih Provider</option>
+                                    <option value="btn">BTN</option>
+                                    <option value="telkom">Telkom</option>
+                                    <option value="lintasarta">Lintasarta</option>
+                                    <option value="tigatra">Tigatra</option>
+                                    <option value="primalink">Primalink</option>
+                                    <option value="ipwan">IPWAN</option>
+                                    <option value="iforte">IForte</option>
+                                    <option value="mile">MILE</option>
+                                    <option value="bas">BAS</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Hak Akses</label>
+                                <select
+                                    class="form-control text-sm <?php if (session('errors.hak_akses')) : ?>is-invalid<?php endif ?>"
+                                    name="hak_akses" id="hak_akses" value="<?= $p['hak_akses']; ?>"
+                                    style="width: 100%;">
+                                    <option disabled="disabled" selected="selected">Pilih Hak Akses</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">User</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer d-flex bd-highlight">
+                    <button type="button" class="btn btn-danger mr-auto" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Ubah</button>
+                    <!-- swalSaveSuccess -->
+                </div>
+            </form>
         </div>
     </div>
 </div>
+<!-- End Modal Edit -->
 <?= $this->endSection(); ?>
 
 <?= $this->section('script'); ?>
