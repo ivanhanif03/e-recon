4deaa1d655bbc0e4f9f5583533349f47c29b876e
ydhmakhuta <yudhamakhuta@gmail.com>
2022-11-16 17:52:32 +0700

    add migrate

--- app/Database/Migrations/2022-11-16-082558_Stopclock.php
+++ app/Database/Migrations/2022-11-16-082558_Stopclock.php
@@ -5,14 +5,30 @@ namespace App\Database\Migrations;
 use CodeIgniter\Database\Migration;
 
 class Stopclock extends Migration
-{
-    public function up()
+public function up()
     {
-        //
+        $this->forge->addField([
+			'no_tiket'          => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+				'unsigned'       => true,
+				'auto_increment' => true
+			],
+			'start_pause'       => [
+				'type'           => 'DATETIME',
+				'constraint'     => '10'
+			],
+			'nama_regional'      => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255'
+            ]
+
+		]);
+        	
+        // Membuat primary key
+		$this->forge->addKey('id', TRUE);
+
+		// Membuat tabel 
+		$this->forge->createTable('Regional', TRUE);
     }
 
-    public function down()
-    {
-        //
-    }
-}
