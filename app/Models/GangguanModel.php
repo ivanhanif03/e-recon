25a4386d0e6481f2e073a3fd2d24419e4572b673
shafiranuradya <shafiranuradya@gmail.com>
2022-11-17 14:44:03 +0700

    up

--- app/Models/GangguanModel.php
+++ app/Models/GangguanModel.php
@@ -6,12 +6,12 @@ use CodeIgniter\Model;
 
 class GangguanModel extends Model 
 {
-    protected $table = 'tb_gangguan';
+    protected $table = 'gangguan';
     protected $useTimestamps = true;
-    protected $allowedfields = [
-        'provider',
-        'outlet',
-        'PIC',
-        'alamat',
-    ];
+    protected $allowedfields = ['nomor_tiket', 'nama_gangguan', 'id_branch', 'id_provider', 'id_regional', 'detail', 'start', 'end', 'status', 'approval'];
+
+public function getGangguan()
+{
+    return $this->db->table('gangguan');
+}
 }
