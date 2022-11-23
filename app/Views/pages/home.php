3906f32c376bf3dbd65f15c494c9885e20abcfaa
ydhmakhuta <yudhamakhuta@gmail.com>
2022-11-23 16:04:21 +0700

    push terossss

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -11,10 +11,6 @@
                     <h1 class="m-0">Dashboard</h1>
                 </div>
                 <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">E-Recon</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
-                    </ol>
                 </div>
             </div>
         </div>
