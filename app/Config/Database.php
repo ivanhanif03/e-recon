05b1cb053c7e0b9793cec8bfd81f36fa2eb07946
finopse <finopse@gmail.com>
2022-11-23 08:45:35 +0700

    update database isla

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'e-recon',
+        'database' => 'islan',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
